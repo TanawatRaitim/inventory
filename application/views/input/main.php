e8c75413c04855b5caaa2ae595bdf86e5507a28c
TanawatRaitim <tanardroid@gmail.com>
2014-06-06 17:03:40 +0700

    Initial Project

--- application/views/input/main.php
+++ application/views/input/main.php
@@ -25,36 +25,29 @@
 				    <div class="col-md-12">
 				    	<form class="form-horizontal" role="form">
 						  <div class="form-group">
-						    <label for="inputEmail3" class="col-sm-4 control-label">Product ID</label>
+						    <label for="inputEmail3" class="col-sm-4 control-label">รหัสสินค้า</label>
 						    <div class="col-sm-6">
 						      <input type="text" class="form-control input-sm" id="inputEmail3" placeholder="Product ID">
 						    </div>
 						  </div>
-						  <!-- 
-						  <div class="form-group">
-						    <div class="col-sm-2 text-center">ดี</div>
-						    <div class="col-sm-2 text-center">เสีย</div>
-						    <div class="col-sm-2 text-center">ชำรุด</div>
-						    <div class="col-sm-2 text-center">รวม</div>
-						</div>
-						 -->
-						<div class="form-group">   
-						    <div class="col-sm-2">
-						    	<input type="text" class="form-control input-sm" id="inputEmail3" placeholder="ดี">
-						    </div>
-						    <div class="col-sm-2">
-						    	<input type="text" class="form-control input-sm" id="inputEmail3" placeholder="เสีย">
-						    </div>
-						    <div class="col-sm-2">
-						    	<input type="text" class="form-control input-sm" id="inputEmail3" placeholder="ชำรุด">
-						    </div>
-						    <div class="col-sm-2">
-						    	<input type="text" class="form-control input-sm" id="inputEmail3" placeholder="รวม">
-						    </div>
-						    <div class="col-sm-4">
-						    	<button type="submit" class="btn btn-danger btn-sm">&nbsp;&nbsp;เพิ่ม&nbsp;&nbsp;</button>
-						    </div>
-						  </div>
+						
+						<table>
+							<tr>
+								<th class="text-center">ดี</th>
+								<th class="text-center">เสีย</th>
+								<th class="text-center">ชำรุด</th>
+								<th class="text-center">รวม</th>
+								<th class="text-center"></th>
+							</tr>
+							<tr>
+								<td><input type="text" class="col-md-11 col-sm-5 col-xs-8" placeholder="ดี" /></td>
+								<td><input type="text" class="col-md-11 col-sm-5 col-xs-8" placeholder="เสีย" /></td>
+								<td><input type="text" class="col-md-11 col-sm-5 col-xs-8" placeholder="ชำรุด" /></td>
+								<td><input type="text" class="col-md-11 col-sm-5 col-xs-8" placeholder="รวม" /></td>
+								<td><input type="button" class="btn btn-success" value="เพิ่ม" /></td>
+							</tr>
+						</table>
+						  
 						</form>
 				    </div>			    
 				  </div>
@@ -64,7 +57,7 @@
 			<div class="col-md-6">
 				<div class="panel panel-default">
 				  <div class="panel-heading">
-				    <h3 class="panel-title">Product Detail</h3>
+				    <h3 class="panel-title">รายละเอียดสินค้า</h3>
 				  </div>
 				  <div class="panel-body">
 					<div class="col-md-4">
@@ -96,7 +89,7 @@
 						</table>
 					</div>
 					<div class="col-md-12">
-						<table class="table table-condensed">
+						<table class="table table-condensed table-hover">
 							<thead>
 								<tr>
 									<th colspan="2">รายละเอียดสินค้า</th>
@@ -106,6 +99,50 @@
 								<tr>
 									<th>รหัส</th>
 									<td>13-GMAG-M1375</td>
+								</tr>
+								<tr>	
+									<th>ชื่อ</th>
+									<td>The Guitar Mag</td>
+								</tr>
+								<tr>	
+									<th>Vol.</th>
+									<td></td>
+								</tr>
+								<tr>	
+									<th>คลังหลัก</th>
+									<td>C</td>
+								</tr>
+								<tr>	
+									<th>พิมพ์ครั้งที่</th>
+									<td>1</td>
+								</tr>
+								<tr>	
+									<th>ยอดสั่งผลิต</th>
+									<td>2,000</td>
+								</tr>
+								<tr>	
+									<th>รับเข้าคลัง</th>
+									<td>1,800</td>
+								</tr>
+								<tr>	
+									<th>ยอดกระจาย(SA)</th>
+									<td></td>
+								</tr>
+								<tr>	
+									<th>ยอดกระจาย(SC)</th>
+									<td></td>
+								</tr>
+								<tr>	
+									<th>วันที่ผลิต</th>
+									<td></td>
+								</tr>
+								<tr>	
+									<th>ผลิตเสร็จ</th>
+									<td></td>
+								</tr>
+								<tr>	
+									<th>สถานะ</th>
+									<td></td>
 								</tr>
 							</tbody>
 						</table>
@@ -118,9 +155,10 @@
 		<div class="col-md-4">
 			<div class="panel panel-default">
 			  <div class="panel-heading">
-			    <h3 class="panel-title">Ticket Information</h3>
+			    <h3 class="panel-title">รายการที่บันทึก</h3>
 			  </div>
 			  <div class="panel-body">
+			  	
 				  <table class="table table-condensed table-border table-striped">
 			    	<thead>
 			    		<tr>
@@ -131,7 +169,7 @@
 			    			<th>เสีย</th>
 			    			<th>ชำรุด</th>
 			    			<th>Total</th>
-			    			<th>Action</th>
+			    			<th></th>
 			    		</tr>
 			    	</thead>
 			    	<tbody>
@@ -143,20 +181,59 @@
 			    			<td>20</td>
 			    			<td>30</td>
 			    			<td>65</td>
-			    			<td>action</td>
+			    			<td>
+			    				<!-- Single button -->
+								<div class="btn-group">
+								  <button type="button" class="btn btn-xs btn-default dropdown-toggle" data-toggle="dropdown">
+								    Action <span class="caret"></span>
+								  </button>
+								  <ul class="dropdown-menu pull-right" role="menu">
+								    <li><a href="#">แก้ไข</a></li>
+								    <li><a href="#">ลบ</a></li>
+								  </ul>
+								</div>
+								<!-- Single button -->
+			    			</td>
+			    		</tr>
+			    		<tr>
+			    			<td>2</td>
+			    			<td>GMAG</td>
+			    			<td>15</td>
+			    			<td>15</td>
+			    			<td>20</td>
+			    			<td>30</td>
+			    			<td>65</td>
+			    			<td>
+			    				<!-- Single button -->
+								<div class="btn-group">
+								  <button type="button" class="btn btn-xs btn-default dropdown-toggle" data-toggle="dropdown">
+								    Action <span class="caret"></span>
+								  </button>
+								  <ul class="dropdown-menu pull-right" role="menu">
+								    <li><a href="#">แก้ไข</a></li>
+								    <li><a href="#">ลบ</a></li>
+								  </ul>
+								</div>
+								<!-- Single button -->
+			    			</td>
 			    		</tr>
 			    	</tbody>
 			    	<tfoot>
 			    		<tr>
 			    			<td colspan="3" class="text-center">รวม</td>
-			    			<td>375</td>
-			    			<td>20</td>
 			    			<td>30</td>
-			    			<td>65</td>
-			    			<td>action</td>
+			    			<td>40</td>
+			    			<td>60</td>
+			    			<td>130</td>
+			    			<td></td>
 			    		</tr>
 			    	</tfoot>
 			    </table>
+			    <div class="col-md-6 col-md-offset-3">
+			    	<button class="btn btn-primary btn-sm">บันทึก Ticket</button>
+			  		<button class="btn btn-danger btn-sm">ยกเลิกการบันทึก Ticket</button>
+			    </div>
+			    
 			  </div>
 			</div>
 		</div>
